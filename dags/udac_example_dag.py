dcc13139a7a189f6b8ccb728e8686a9990705acb
Runkang Shi <srkwhu@gmail.com>
2020-06-23 18:20:22 +0000

    implementing the first block in the DAG

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -9,15 +9,20 @@ from helpers import SqlQueries
 # AWS_KEY = os.environ.get('AWS_KEY')
 # AWS_SECRET = os.environ.get('AWS_SECRET')
 
+#For prototype, we do not need retry function.
 default_args = {
     'owner': 'udacity',
     'start_date': datetime(2019, 1, 12),
+#     'retries': 3,
+#     'retry_delay':timedelta(minutes=5),
+#     'email_on_retry': False
 }
 
 dag = DAG('udac_example_dag',
+#           catchup=False,
           default_args=default_args,
-          description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
+#           description='Load and transform data in Redshift with Airflow',
+#           schedule_interval='0 * * * *'
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
@@ -39,7 +44,11 @@ load_songplays_table = LoadFactOperator(
 
 load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
-    dag=dag
+    dag=dag,
+    table_name='stage_events',
+    s3_link='s3://udacity-dend/log_data',
+    redshift_id='redshift',
+    credentials='credentials'
 )
 
 load_song_dimension_table = LoadDimensionOperator(
@@ -63,3 +72,18 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+load_song_dimension_table >> run_quality_checks
+load_user_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+run_quality_checks >> end_operator
--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,26 +1,55 @@
 from airflow.hooks.postgres_hook import PostgresHook
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
+    
+    import_s3_song="""COPY {table_name} FROM '{s3_link}'
+                 CREDENTIALS 'aws_iam_role=arn:aws:iam::045574897248:role/myRedshiftRole'
+                 REGION 'us-west-2'
+                 TIMEFORMAT 'epochmilisecs'
+                 FORMAT AS JSON 'auto'"""
+    
+    import_s3_event="""COPY {table_name} FROM '{s3_link}'
+                       CREDENTIALS 'aws_iam_role=arn:aws:iam::045574897248:role/myRedshiftRole'
+                       TIMEFORMAT as 'epochmillisecs'
+                       TRUNCATECOLUMNS BLANKSASNULL EMPTYASNULL
+                       REGION 'us-west-2'
+                       FORMAT AS JSON 's3://udacity-dend/log_json_path.json'"""
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 table_name='',
+                 s3_link='',
+                 redshift_id='',
+                 credentials='',
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
-
-    def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
-
-
-
-
-
+        self.table_name=table_name
+        self.s3_link=s3_link
+        self.redshift_id=redshift_id
+        self.credentials=credentials
+#This part of function returns error indicator: execute() got an unexpected keyword argument 'context'
+#Is the 'context' parameter mandatory?
+#What if I just want to use s3 link as indicator?
+    def execute(self, s3_link):
+        aws_hook=AwsHook(self.credentials)
+        credentials=aws_hook.get_credentials()
+        redshift=PostgresHook(postgres_conn_id=redshift_id)
+        self.log.info("Clearing.")
+        redshift.run("DELETE FROM {table_name}").format(self.table_name)
+        
+        self.log.info("Importing.")
+        if s3_link == 's3://udacity-dend/log_data':
+            sql=StageToRedshiftOperator.import_s3_event.format(
+                                        table_name=self.table_name,
+                                        s3_link=self.s3_link)
+            redshift.run(sql)
+        elif s3_link == 's3://udacity-dend/song_data':
+            sql=StageToRedshiftOperator.import_s3_song.format(
+                                        table_name=self.table_name,
+                                        s3_link=self.s3_link)
+            redshift.run(sql)
